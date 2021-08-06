44dec05c35094df7ddbf77f2dff5ecd3e12075c4
Jong-Ryul Lee <jongryul.lee@etri.re.kr>
2021-08-06 21:06:22 +0900

    base interface
    Signed-off-by: Jong-Ryul Lee <jongryul.lee@etri.re.kr>

--- nncompress/mm/nnmm.py
+++ nncompress/mm/nnmm.py
@@ -1,1 +1,22 @@
 """ nnmm """
+
+class NNMM(object):
+
+    def __init__(self):
+        pass
+
+    def build(self, search_space):
+        """ build """
+        pass
+
+    def train(self):
+        """ sub-network training"""
+        pass
+
+    def evaluate(self):
+        """ sub-network evaluation"""
+        pass
+
+    def retrieve(self, spec):
+        """ sub-network retrieval"""
+        pass
